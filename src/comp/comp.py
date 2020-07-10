b04874695c4bd7b7f6f6c8451348c41d96b5b6d3
Michael Callahan <CallahanMichaelE@gmail.com>
2020-07-10 10:17:21 -0600

    f I beleive is passing

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -54,8 +54,9 @@ print(e)
 # age, for example ("David", 31), for everyone between the ages of 27 and 32,
 # inclusive.
 print("Names and ages between 27 and 32:")
-f = []
+f = [(x.name, x.age) for x in humans]
 print(f)
+print(type(f[0]))
 
 # Write a list comprehension that creates a list of new Humans like the old
 # list, except with all the names uppercase and the ages with 5 added to them.
