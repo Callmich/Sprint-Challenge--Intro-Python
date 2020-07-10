c3274df2b97dc8e4cba72cbc6c26b8bdd9a706d9
Michael Callahan <CallahanMichaelE@gmail.com>
2020-07-10 11:29:00 -0600

    need to find a way to skip the first line

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -10,7 +10,7 @@ class City:
     self.lon = lon
   
   def __str__(self):
-    return F'{self.name}'
+    return F'{self.name}, {self.lat}, {self.lon}'
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
@@ -32,15 +32,15 @@ def cityreader(cities=[]):
   # For each city record, create a new City instance and add it to the 
   # `cities` list
   
-  with open("src/cityreader/cities.csv", "r") as cvsFile:
+  with open("cities.csv", "r") as cvsFile:
     csvReader = csv.reader(cvsFile, delimiter=',')
     lineCount = 0
     for row in csvReader:
-      # if lineCount == 0:
-      #   pass
-      # else:
-      cities.append(City(row[0], row[3], row[4]))
-      lineCount += 1
+      if lineCount == 0:
+        next
+      else:
+        cities.append(City(row[0], row[3], row[4]))
+        lineCount += 1
     
     return cities
 
