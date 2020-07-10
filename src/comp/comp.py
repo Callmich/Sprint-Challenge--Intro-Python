54bac848121af3f2aa70543456339207713e2332
Michael Callahan <CallahanMichaelE@gmail.com>
2020-07-10 10:38:15 -0600

    g is now solved in comp and f updated to limit the range

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -54,7 +54,7 @@ print(e)
 # age, for example ("David", 31), for everyone between the ages of 27 and 32,
 # inclusive.
 print("Names and ages between 27 and 32:")
-f = [(x.name, x.age) for x in humans]
+f = [(x.name, x.age) for x in humans if x.age > 26 and x.age < 32]
 print(f)
 print(type(f[0]))
 
@@ -62,7 +62,13 @@ print(type(f[0]))
 # list, except with all the names uppercase and the ages with 5 added to them.
 # The "humans" list should be unmodified.
 print("All names uppercase:")
-g = []
+g = [Human(x.name.upper(), x.age + 5) for x in humans]
+# g=[]
+# for x in humans:
+#     x.name.upper()
+#     x.age + 5
+#     g.append(x)
+     
 print(g)
 
 # Write a list comprehension that contains the square root of all the ages.
